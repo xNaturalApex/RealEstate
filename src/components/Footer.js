1a283b1e2b29a1bab14ea00f5338f274ed5d3d41
xNaturalApex <130421088+xNaturalApex@users.noreply.github.com>
2023-09-30 19:39:18 -0400

    Funtions Added pt11

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,7 +15,7 @@ function Footer() {
 
     try {
       const response = await axios.post(
-        '/.netlify/functions/submitForm',
+        '/.netlify/functions/Functions/submitForm', // Adjust the path to your function
         formData
       );
       if (response.status === 200) {
