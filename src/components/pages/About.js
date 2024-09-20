9480ad5e119d75ae558e8b434814b1784fec9ef6
Felipe Franco <felipe@Felipes-MacBook-Pro.local>
2024-09-19 22:10:46 -0400

    First BIR commit pt2

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import '../../App.css';
-import Footer from '../Footer';
-import AboutSection from '../AboutSection';
-import '../TrainerCard.css';
-import '../AboutSection.css';
-import Testimonials from '../Testimonial';
+import React from "react";
+import "../../App.css";
+import Footer from "../Footer";
+import AboutSection from "../AboutSection";
+import "../AboutSection.css";
+import Testimonials from "../Testimonial";
 
 function About() {
   return (
